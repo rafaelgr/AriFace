2a6f864f48880d29bb4eaa6ef52c57fba9b6a288
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2015-01-12 17:16:31 +0100

    Help de Cliente

--- SQL/tablas_facturae_ariadna.sql
+++ SQL/tablas_facturae_ariadna.sql
@@ -30,20 +30,6 @@ CREATE TABLE `plantilla` (
 
 insert  into `plantilla`(`contenido`,`nombre`,`plantilla_id`,`observaciones`) values ('<p><img alt=\"Teinsa S.L.\" src=\"http://www.teinsa.es/s/cc_images/cache_12097161.jpg?t=1360606162\" style=\"height:57px; width:210px\" /></p>\n\n<p>Estimado cliente, {0}</p>\n\n<p>Ya se encuentran disponibles las siguientes facturas:</p>\n\n<p>{1}</p>\n\n<p>Que le adjuntamos en este correo. Le recordamos que en nuestra web de facturaci&oacute;n electr&oacute;nica&nbsp;<a href=\"http://facturae.teinsa.es\">http://facturae.teinsa.es</a>&nbsp;siempre podr&aacute; consultar tanto estas como otras emitidas.</p>\n\n<p>Atentamente.</p>\n\n<p>Teinsa S.L.</p>\n','Envio email fact. electronica',1,'{0} Cliente </br>\r\n{1} Detalles de factura </br>\r\n');
 
-/*Table structure for table `repositorio` */
-
-DROP TABLE IF EXISTS `repositorio`;
-
-CREATE TABLE `repositorio` (
-  `path` varchar(255) DEFAULT NULL,
-  `i_d` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`i_d`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
-/*Data for the table `repositorio` */
-
-insert  into `repositorio`(`path`,`i_d`) values ('C:\\Intercambio\\Repositorio\\',1);
-
 /*Table structure for table `sistema` */
 
 DROP TABLE IF EXISTS `sistema`;
@@ -59,25 +45,6 @@ CREATE TABLE `sistema` (
 /*Data for the table `sistema` */
 
 insert  into `sistema`(`sistema_id`,`nombre`,`base_datos`,`descripcion`) values (1,'AriGes','ariges1','ARIGES'),(2,'AriGasol','arigasol','ARIGASOL'),(3,'AriAgro','ariagro8','ARIAGRO'),(4,'AriGesDos','ariges6','TELEFONIA'),(5,'AriTaxi','aritaxi','TELETAXI'),(6,'AriGes','ariges4','ARIGESCON');
-
-/*Table structure for table `superusuario` */
-
-DROP TABLE IF EXISTS `superusuario`;
-
-CREATE TABLE `superusuario` (
-  `Login` varchar(255) DEFAULT NULL,
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Id_empresa` int(11) DEFAULT NULL,
-  `Email` varchar(255) DEFAULT NULL,
-  `Contrasena` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `idx_superusuario_Id_empresa` (`Id_empresa`),
-  CONSTRAINT `ref_superusuario_empresa` FOREIGN KEY (`Id_empresa`) REFERENCES `empresa` (`id_empresa`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
-/*Data for the table `superusuario` */
-
-insert  into `superusuario`(`Login`,`Id`,`Id_empresa`,`Email`,`Contrasena`) values ('admin',1,1,'rafa@myariadna.com','admin');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
